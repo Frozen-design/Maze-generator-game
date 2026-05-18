efbf76eba1672189d15b3bbaabc23053398bf916
Frozen-design <daniel.l.melia@gmail.com>
2026-05-18 17:25:14 -0600

    Added QuadTree data structure to main for quicker collision detection. Added interaction with the circles too

--- physics.py
+++ physics.py
@@ -1,17 +1,24 @@
+from __future__ import annotations
 import numpy as np
 import pygame
 import math
+from shapely.geometry import Point, LineString
+
+# 90 degrees clockwise
+def normal2D(vector):
+    return np.array([-vector[1], vector[0]])
 
 class Main:
     def __init__(self, fps) -> None:
         self.fps = fps
-        self.steps = 10
+        self.steps = 5
         self.dt = 1 / (self.fps * self.steps)
         self.gravity = np.array([0.0, 360.0])
         self.lines = [Line2D((0, 0), (1000, 0)), Line2D((1000, 1000), (-1000, 0)), Line2D((1000, 0), (0, 1000)), Line2D((0, 1000), (0, -1000))]
-        self.balls = []
-        self.springs = []
+        self.balls:list[Ball2D] = []
+        self.springs:list[Spring] = []
         self.friction = 0.001
+        self.quadtree:QuadTree | None = None
         pass
 
     def add_polygon(self, polygon):
@@ -22,12 +29,86 @@ class Main:
         self.balls.append(ball)
 
     def simulate(self):
+        wh = pygame.display.get_window_size()
         for _ in range(self.steps):
+            qt = QuadTree(Rect2D((wh[0]//2, wh[1]//2), wh[0]//2, wh[1]//2), 2)
             for i in range(len(self.balls)):
-                self.balls[i].simulate()
+                b = self.balls[i]
+                qt.insert(Point2D(b.xy[0], b.xy[1], b))
+                b.simulate(qt)
+            self.quadtree = qt
 
             for i in self.springs:
                 i.simulate()
+
+class Point2D:
+    def __init__(self, x, y, data) -> None:
+        self.x = x
+        self.y = y
+        self.data = data
+        pass
+
+class Rect2D:
+    def __init__(self, center, width, height) -> None:
+        self.center = center
+        self.x, self.y = center
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(0, 0, self.width*2, self.height*2)
+        self.rect.center = self.center
+    
+    def intersects(self, other: Rect2D):
+        return self.rect.colliderect(other.rect)
+    
+    def contains(self, point:Point2D):
+        return self.rect.collidepoint((point.x, point.y))
+
+class QuadTree:
+    def __init__(self, boundary:Rect2D, capacity) -> None:
+        self.boundary = boundary
+        self.x, self.y = boundary.x, boundary.y
+        self.width, self.height = boundary.width, boundary.height
+        self.capacity = capacity
+        self.points:list[Point2D] = []
+        self.divided = False
+        #topleft, topright, bottomright, bottomleft
+        self.quadtrees = []
+
+    def divide(self):
+        self.divided = True
+        topleft = QuadTree(Rect2D((self.x - self.width/2, self.y - self.height/2), self.width/2, self.height/2), self.capacity)
+        topright = QuadTree(Rect2D((self.x + self.width/2, self.y - self.height/2), self.width/2, self.height/2), self.capacity)
+        bottomright = QuadTree(Rect2D((self.x + self.width/2, self.y + self.height/2), self.width/2, self.height/2), self.capacity)
+        bottomleft = QuadTree(Rect2D((self.x - self.width/2, self.y + self.height/2), self.width/2, self.height/2), self.capacity)
+        self.quadtrees = [topleft, topright, bottomright, bottomleft]
+    
+    def insert(self, point):
+        if self.boundary.contains(point) == False:
+            return
+        if len(self.points) < self.capacity:
+            self.points.append(point)
+        else:
+            if not self.divided:
+                self.divide()
+            for i in self.quadtrees:
+                i.insert(point)
+
+    def query(self, range:Rect2D, point_list:list[Point2D]):
+        if self.boundary.intersects(range):
+            for p in self.points:
+                point_list.append(p)
+            if self.divided:
+                for qt in self.quadtrees:
+                    qt.query(range, point_list)
+            return True
+        else:
+            return False
+    
+    def show(self, surface):
+        pygame.draw.rect(surface, "white", self.boundary.rect, width = 2)
+        if self.divided:
+            for i in self.quadtrees:
+                i.show(surface)
 
 class Line2D:
     def __init__(self, point, direction) -> None:
@@ -62,17 +143,17 @@ class Ball2D:
         self.radius = radius
         self.velocity = np.array([0.0 for _ in range(len(self.xy))])
         self.restitution = 0.99
-
-        pass
     
-    def simulate(self):
-        
+    def simulate(self, qt:QuadTree):
         self.velocity += self.main.gravity * self.main.dt
         for line in self.main.lines:
             self.line_collision(line)
-        for ball in self.main.balls:
-            if ball is not self:
-                self.ball_collision(ball)
+        points = []
+        qt.query(Rect2D([*self.xy], self.radius*2, self.radius*2), points)
+        for p in points:
+            if p.data is not self:
+                self.ball_collision(p.data)
+        
         self.xy += self.velocity * self.main.dt
 
     def draw(self, surface, color):
@@ -94,8 +175,8 @@ class Ball2D:
         else:
             normal = line.normal
 
-        if np.dot(self.velocity, normal) > 0:
-            normal = -normal 
+        #if np.dot(self.velocity, normal) > 0:
+            #normal = -normal 
 
         direction_norm = np.array([normal[1], -normal[0]])
         distance_from_line = np.dot(p, normal)
@@ -165,21 +246,53 @@ def loop():
     clock = pygame.time.Clock()
     running = True
     main_sim = Main(60)
-    my_polygon = Polygon([[200, 200], [100, 600], [500, 800], [300, 200]])
+    my_polygon = Polygon([[200, 200], [100, 600], [500, 800], [800, 100], [300, 200]])
     main_sim.add_polygon(my_polygon)
-    for i in range(25):
-        main_sim.add_ball(Ball2D(main_sim, (22*i+20, 100.0), 10))
 
-    for i in range(len(main_sim.balls)-1):
-        main_sim.springs.append(Spring(main_sim, main_sim.balls[i], main_sim.balls[i + 1], force = 10000))
+    balls = np.zeros(shape=(3, 20), dtype=object)
+
+    for i in range(balls.shape[1]):
+        for j in range(balls.shape[0]):
+            balls[j, i] = Ball2D(main_sim, (25*i + 200, 25*j + 20.0), 7)
+            main_sim.add_ball(balls[j, i])
+
+    for index, i in np.ndenumerate(balls):
+        directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+        for j in directions:
+            try:
+                y, x = index
+                yd, xd = j
+                ys, xs = balls.shape
+                if 0 <= (y + yd) < ys and 0 <= (x + xd) < xs:
+                    main_sim.springs.append(Spring(main_sim, balls[y, x], balls[y + yd, x + xd], force = 10000, damping=5, thickness=1)) # type:ignore
+            except:
+                pass
     
-
+    n_spring = None
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if main_sim.quadtree != None:
+                    points = []
+                    main_sim.quadtree.query(Rect2D((event.pos[0], event.pos[1]), 10, 10), points)
+                    pc = min(points, key = lambda x: math.dist([x.x, x.y], event.pos))
+                    ball_pc = pc.data
+                    n_ball = Ball2D(main_sim, event.pos, 1)
+                    n_spring = Spring(main_sim, ball_pc, n_ball, length=0.1, force=1000, damping=1)
+                if event.type == pygame.MOUSEBUTTONUP and n_spring != None:
+                    main_sim.springs.remove(n_spring)
+                    n_spring = None
+        if n_spring != None:
+            n_spring.node2.xy = np.array(pygame.mouse.get_pos())
+            main_sim.springs.append(n_spring)
+
         screen.fill("light blue")
         main_sim.simulate()
+        if main_sim.quadtree != None:
+            main_sim.quadtree.show(screen)
         for i in main_sim.balls:
             i.draw(screen, "black")
         for i in main_sim.lines:
@@ -188,6 +301,8 @@ def loop():
             i.draw(screen, "white")
 
         pygame.display.flip()
+        if n_spring != None:
+            main_sim.springs.remove(n_spring)
         clock.tick(60)
 
 if __name__ == "__main__":
